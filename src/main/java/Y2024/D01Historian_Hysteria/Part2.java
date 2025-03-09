7b2d6d38f29088c19f7eab729e08c65fe2d9fbab
Venus Poon <venuspsw@hotmail.com>
2025-03-09 23:06:10 +0100

    part 1 done

--- src/main/java/Y2024/D01Historian_Hysteria/Part2.java
+++
@@ -1,7 +0,0 @@
-package Y2024.D01Historian_Hysteria;
-
-public class Part2 {
-    Part1 part1;
-
-
-}
--- src/main/java/Y2024/D02RedNoseReports/Part1n2.java
+++ src/main/java/Y2024/D02RedNoseReports/Part1n2.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Part1n2 {
     static ArrayList<Integer> report = new ArrayList<>();
@@ -27,7 +28,61 @@ public class Part1n2 {
         }
     }
 
-    public static void main(String[] args) {
+    private static String ifReportIsIncreasingOrDecreasing (ArrayList<Integer> report){
+        int temp = report.get(1)-report.get(0);
+        if (temp>=1 && temp<=3){
+            return "increasing";
+        } else if (temp<=-1 && temp>=-3) {
+            return "decreasing";
+        }
+        return "err";
+    }
+
+    private static boolean ifReportIncreasingBy1To3 (ArrayList<Integer> report){
+        boolean result = false;
+        for (int i = 0; i < report.size()-1; i++) {
+            int diff = report.get(i+1)-report.get(i);
+            if (diff>=1 && diff<=3){
+                result = true;
+            } else {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    private static boolean ifReportDecresingBy1To3 (ArrayList<Integer> report){
+        boolean result = false;
+        for (int i = 0; i < report.size()-1; i++) {
+            int diff = report.get(i+1)-report.get(i);
+            if (diff<=-1 && diff>=-3){
+                result = true;
+            } else {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    private static int count1ifReportSafe (ArrayList<Integer> report, String status){
+        switch (status){
+            case "increasing": return ifReportIncreasingBy1To3 (report) ? 1 : 0;
+            case "decreasing": return ifReportDecresingBy1To3 (report) ? 1 : 0;
+            default: return 0;
+        }
+    }
+
+    private static int countTotalSafeReports (ArrayList<ArrayList<Integer>> reportList){
         fileReader("src/main/java/Y2024/D02RedNoseReports/Input");
+        int count = 0;
+        for (int i = 0; i < reportList.size(); i++) {
+            String status = ifReportIsIncreasingOrDecreasing(reportList.get(i));
+            count += count1ifReportSafe(reportList.get(i),status);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countTotalSafeReports(reportList));
     }
 }
