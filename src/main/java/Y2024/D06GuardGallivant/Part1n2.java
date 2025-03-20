7367522bdc7e3295c5b28f0f5b300f9cfffed64b
Venus Poon <venuspsw@hotmail.com>
2025-03-21 00:14:23 +0100

    day 6 create files

---
+++ src/main/java/Y2024/D06GuardGallivant/Part1n2.java
@@ -0,0 +1,10 @@
+package Y2024.D06GuardGallivant;
+
+public class Part1n2 {
+    //find the start location
+    //check if every location is qualified to win the game
+    //check if there's obstacles at the front
+    //change direction
+    //mark "X"
+    //check all "X"
+}
