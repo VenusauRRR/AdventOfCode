1c7af74c9b1a2e260be99b6bc98d1493b783ada8
Venus Poon <venuspsw@hotmail.com>
2026-01-16 20:17:40 +0100

    bug still

--- src/main/java/Y2025/D01SecretEntrance/main.cpp
+++ src/main/java/Y2025/D01SecretEntrance/main.cpp
@@ -1,64 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
-void part1_rotateTheSafe(int &init, int &counter, char direction, std::string move)
-{   
-    int steps = std::stoi(move);
-    if (direction == 'L'){
-        init -= steps%100;
-        if (init < 0) init += 100;
-    } else {
-        init += steps%100;
-        if (init > 99) init -= 100;
-    }
-    // std::cout << init << std::endl;
-    if (init == 0) counter++;
+// void part1_rotateTheSafe(int &init, int &counter, char direction, std::string move)
+// {   
+//     int steps = std::stoi(move);
+//     if (direction == 'L'){
+//         init -= steps%100;
+//         if (init < 0) init += 100;
+//     } else {
+//         init += steps%100;
+//         if (init > 99) init -= 100;
+//     }
+//     // std::cout << init << std::endl;
+//     if (init == 0) counter++;
 
+// }
+
+bool isZero(int number){
+    return number==0;
 }
 
 void part2_rotateTheSafeAndCountClicks(int &init, int &counter, char direction, std::string move)
 {
-    int temp = init;
-    std::cout << "init: " << temp << std::endl;
+    int origin = init;
+    std::cout << "origin: " << init << std::endl;
     int steps = std::stoi(move);
-    std::cout << "step: " << steps << std::endl;
-    if (direction == 'L')
-    {
+    std::cout << "steps: " << steps << std::endl;
+    if (direction == 'L'){
         init -= steps;
-        if (init < 0)
-        {
-            counter += 1 + (steps - temp) / 100;
-            if (temp == 0)
-            {
+        std::cout << "after steps: " << init << std::endl;
+        if (init > 0){
+            std::cout << "init >0: " << init << std::endl;
+            return;
+        } else if (init%100 == 0){
+            counter = counter + 1 + (init/100);
+
+            std::cout << "couter when init=0: " << counter << std::endl;
+        } else {
+            // if (init < 0)
+            
+            counter = counter + 1 + ((steps-origin)/100);
+            if (origin==0){
                 counter--;
             }
-            std::cout << "count L: " << counter << std::endl;
             init %= 100;
             init += 100;
+            std::cout << "init<0: " << init << std::endl;
+            std::cout << "counter when init<0: " << counter << std::endl;
         }
-    }
-    else
-    {
+    } else {
         init += steps;
-        if (init > 99)
+        std::cout << "after steps: " << init << std::endl;
+        if (init < 100){
+            std::cout << "init<100: " << init << std::endl;
+            return;
+        } else if (init%100==0)
         {
-            std::cout << "here init: " << init << std::endl;
-            std::cout << "count here: " << counter << std::endl;
-            counter += 1 + (steps - (100 - temp)) / 100;
-            if (init == 100){
-                counter--;
-            }
-            std::cout << "count R: " << counter << std::endl;
-            init %= 100;
+            counter += init/100;
+            init = 0;
+            std::cout << "couter when init=0: " << counter << std::endl;
+            std::cout << "init=0: " << init << std::endl;
+        } else {
+            // if (init > 100)
+            counter = counter + init/100;
+            init = init%100;
+            std::cout << "init>0: " << init << std::endl;
+            std::cout << "counter when init>0: " << counter << std::endl;
         }
     }
-    if (init == 0)
-    {
-        counter++;
-        std::cout << "count at 0: " << counter << std::endl;
-    }
-    std::cout << "after: " << init << std::endl;
 }
 
 int main(void)
@@ -73,8 +84,8 @@ int main(void)
         std::cout << "open success" << std::endl;
         while (std::getline(inFile, fileLine))
         {
-            part1_rotateTheSafe(init, countLeftPointAt0, fileLine.at(0), fileLine.substr(1));
-            // part2_rotateTheSafeAndCountClicks(init, countLeftPointAt0, fileLine.at(0), fileLine.substr(1));
+            // part1_rotateTheSafe(init, countLeftPointAt0, fileLine.at(0), fileLine.substr(1));
+            part2_rotateTheSafeAndCountClicks(init, countLeftPointAt0, fileLine.at(0), fileLine.substr(1));
         }
     }
     std::cout << "Total count for Point to 0: " << countLeftPointAt0 << std::endl;
