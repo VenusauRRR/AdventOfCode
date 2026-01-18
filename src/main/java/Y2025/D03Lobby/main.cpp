1ef978ce1ec4d9bae0fc9486322dbc617d5e0de4
Venus Poon <venuspsw@hotmail.com>
2026-01-18 10:08:39 +0100

    refine main() with findMaxJoltageIndex()

--- src/main/java/Y2025/D03Lobby/main.cpp
+++ src/main/java/Y2025/D03Lobby/main.cpp
@@ -3,13 +3,14 @@
 #include <string>
 #include <vector>
 
-int findMaxJoltageIndex(std::string input, std::string &digit){
-    // std::cout << "input: " << input << std::endl;
+int findMaxJoltageIndex(std::string input, std::string &digit)
+{
     int index = -1;
     for (size_t i = 9; i >= 0; i--)
     {
         index = input.find(std::to_string(i));
-        if (index >= 0){
+        if (index >= 0)
+        {
             digit = std::to_string(i);
             return index;
         }
@@ -17,32 +18,33 @@ int findMaxJoltageIndex(std::string input, std::string &digit){
     return -1;
 }
 
-int main(void){
+int findMaxJoltage(std::string input)
+{
+    std::string firstDigit, secondDigit;
+    int firstIndex = findMaxJoltageIndex(input.substr(0, input.length() - 1), firstDigit);
+    std::string temp = input.substr(firstIndex + 1);
+    int secondeIndex = findMaxJoltageIndex(temp, secondDigit);
+    int maxJoltage = std::stoi(firstDigit + secondDigit);
+    return maxJoltage;
+}
+
+int main(void)
+{
     std::ifstream inFile("input.txt");
     std::string fileLine;
     std::vector<int> joltageList;
 
-    if (inFile.is_open()){
+    if (inFile.is_open())
+    {
         std::cout << "file open success" << std::endl;
-        while (std::getline(inFile,fileLine))
+        while (std::getline(inFile, fileLine))
         {
-            // std::cout << fileLine << std::endl;
-            std::string firstDigit, secondDigit;
-            // std::string temp = fileLine.substr(5);
-            int firstIndex = findMaxJoltageIndex(fileLine.substr(0,fileLine.length()-1), firstDigit);
-            // std::cout << "first index: " << firstIndex << std::endl;
-            std::string temp = fileLine.substr(firstIndex+1);
-            // std::cout << "temp: " << temp << std::endl;
-            int secondeIndex = findMaxJoltageIndex(temp, secondDigit);
-            // std::cout << "index: " << firstIndex << ", 1st digit: " << firstDigit << ", 2nd digit: " << secondDigit << std::endl;
-            int maxJoltage = std::stoi(firstDigit+secondDigit);
-            // std::cout << maxJoltage << std::endl;
-            joltageList.push_back(maxJoltage);
+            joltageList.push_back(findMaxJoltage(fileLine));
         }
-        
     }
     long sum = 0;
-    for(int it : joltageList){
+    for (int it : joltageList)
+    {
         sum += it;
     }
     std::cout << "Total joltage: " << sum << std::endl;
