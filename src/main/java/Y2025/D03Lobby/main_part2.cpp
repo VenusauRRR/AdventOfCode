e17774e48e5ec614fae50f296828f0c93130d71f
Venus Poon <venuspsw@hotmail.com>
2026-01-19 09:36:42 +0100

    seperate part 1 and 2 for Y2025 Day 3

--- src/main/java/Y2025/D03Lobby/main_part2.cpp
+++ src/main/java/Y2025/D03Lobby/main_part2.cpp
@@ -1,32 +1,8 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-
-int part1_findMaxJoltageIndex(std::string input, std::string &digit)
-{
-    int index = -1;
-    for (size_t i = 9; i >= 0; i--)
-    {
-        index = input.find(std::to_string(i));
-        if (index >= 0)
-        {
-            digit = std::to_string(i);
-            return index;
-        }
-    }
-    return -1;
-}
-
-int par1_findMaxJoltage(std::string input)
-{
-    std::string firstDigit, secondDigit;
-    int firstIndex = part1_findMaxJoltageIndex(input.substr(0, input.length() - 1), firstDigit);
-    std::string temp = input.substr(firstIndex + 1);
-    int secondeIndex = part1_findMaxJoltageIndex(temp, secondDigit);
-    int maxJoltage = std::stoi(firstDigit + secondDigit);
-    return maxJoltage;
-}
 
 int part2_findMaxJoltageIndex(std::string input, std::vector<std::string> &list)
 {
@@ -37,7 +13,6 @@ int part2_findMaxJoltageIndex(std::string input, std::vector<std::string> &list)
         if (index >= 0)
         {
             list.push_back(std::to_string(i));
-            // std::cout << "i: " << i << std::endl;
             return index;
         }
     }
@@ -53,9 +28,7 @@ void par2_findMaxJoltage(std::string input, std::vector<std::string> &list, int 
     std::string temp = input.substr(0, input.length() - counter + 1);
     int index = part2_findMaxJoltageIndex(temp, list);
     counter--;
-    // std::cout << "temp: " << temp << ", index: " << index << ", count: " << counter << std::endl;
     std::string trimString = input.substr(index+1);
-    // std::cout << "trim string: " << trimString << std::endl;
     par2_findMaxJoltage(input.substr(index+1), list, counter);
 }
 
@@ -74,15 +47,12 @@ int main(void)
             int count = 12;
             std::vector<std::string> list;
             list.reserve(12);
-            // joltageList.push_back(par1_findMaxJoltage(fileLine));
             par2_findMaxJoltage(fileLine, list, count);
             s = "";
             for (auto it : list)
             {
                 s += it;
-                // std::cout << "it: " << it << std::endl;
             }
-            // std::cout << "s: " << s << std::endl;
             long long maxJoltage = std::stoll(s);
             joltageList.push_back(maxJoltage);
         }
