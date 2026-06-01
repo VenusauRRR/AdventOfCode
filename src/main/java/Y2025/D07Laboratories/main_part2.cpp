587fd9fd4c979a2e33b1a9aafe2e612baabbc450
VenusauRRR <venuspsw@hotmail.com>
2026-06-01 14:52:58 +0200

    x recursion_new: timeout

--- src/main/java/Y2025/D07Laboratories/main_part2.cpp
+++ src/main/java/Y2025/D07Laboratories/main_part2.cpp
@@ -5,8 +5,9 @@
 #include <vector>
 #include <algorithm>
 
-void fileReader(std::vector<std::string> &strList){
-    std::ifstream inFile("testinput.txt");
+void fileReader(std::vector<std::string> &strList)
+{
+    std::ifstream inFile("input.txt");
     std::string fileLine;
     if (inFile.is_open())
     {
@@ -18,11 +19,13 @@ void fileReader(std::vector<std::string> &strList){
     inFile.close();
 }
 
-void findLetterSIndex(std::vector<std::string> &strList, std::vector<int> &idxList, int &idx_init){
+void findLetterSIndex(std::vector<std::string> &strList, std::vector<int> &idxList, int &idx_init)
+{
     int len = strList[0].size();
     for (size_t i = 0; i < len; i++)
     {
-        if (strList[0].at(i) == 'S'){
+        if (strList[0].at(i) == 'S')
+        {
             idx_init = i;
             idxList.push_back(idx_init);
             return;
@@ -30,62 +33,87 @@ void findLetterSIndex(std::vector<std::string> &strList, std::vector<int> &idxLi
     }
 }
 
-bool ifIndexNotExist(int idx, std::vector<int> &idxList){
-        auto it = std::find_if(idxList.begin(),idxList.end(),[idx](int p){
-        return p == idx;
-    });
+bool ifIndexNotExist(int idx, std::vector<int> &idxList)
+{
+    auto it = std::find_if(idxList.begin(), idxList.end(), [idx](int p)
+                           { return p == idx; });
     return it == idxList.end();
 }
 
-void recursion(std::string s, int idx, std::vector<int> &newIdxList, long long &totalTimeline){
+// void recursion(std::string s, int idx, std::vector<int> &newIdxList, long long &totalTimeline)
+// {
+//     int idxSplit_L, idxSplit_R;
+
+//     if (s.at(idx) == '^')
+//     {
+//         idxSplit_L = idx - 1;
+//         idxSplit_R = idx + 1;
+//         if (idxSplit_L >= 0)
+//         {
+//             newIdxList.push_back(idxSplit_L);
+//         }
+//         if (idxSplit_R < s.size())
+//         {
+//             newIdxList.push_back(idxSplit_R);
+//         }
+//     }
+//     else
+//     {
+//         newIdxList.push_back(idx);
+//     }
+// }
+
+void recursion_new(std::vector<std::string> &inputList, int &inputRowSize, int &inputColumnSize, int startRow, int &idx_S_init, long long &totalTimeLine)
+{
+    // std::cout << "i am here" << std::endl;
+    if (startRow > inputRowSize - 1)
+    {
+        return;
+    }
+    // if (totalTimeLine % 2 == 0)
+    // {
+    //     std::cout << totalTimeLine << ", start Row: " << startRow << ", index: " << idx_S_init << std::endl;
+    // }
     int idxSplit_L, idxSplit_R;
 
-    if (s.at(idx) == '^'){
-        idxSplit_L = idx -1;
-        idxSplit_R = idx +1;
-        if (idxSplit_L >= 0){
-            newIdxList.push_back(idxSplit_L);
+    if (inputList.at(startRow).at(idx_S_init) == '^')
+    {
+        idxSplit_L = idx_S_init - 1;
+        idxSplit_R = idx_S_init + 1;
+        totalTimeLine++;
+
+        if (idxSplit_L >= 0)
+        {
+            recursion_new(inputList, inputRowSize, inputColumnSize, startRow + 2, idxSplit_L, totalTimeLine);
         }
-        if (idxSplit_R < s.size()){
-            newIdxList.push_back(idxSplit_R);
+        if (idxSplit_R < inputColumnSize)
+        {
+            recursion_new(inputList, inputRowSize, inputColumnSize, startRow + 2, idxSplit_R, totalTimeLine);
         }
-    } else {
-        newIdxList.push_back(idx);
+    }
+    else
+    {
+        recursion_new(inputList, inputRowSize, inputColumnSize, startRow + 2, idx_S_init, totalTimeLine);
     }
 }
 
-int main(void){
+int main(void)
+{
 
     std::vector<std::string> strList;
     std::vector<int> idxList;
-    long long countSplit = 0;
     int idx_init;
     long long totalTimeline = 1;
 
     fileReader(strList);
     findLetterSIndex(strList, idxList, idx_init);
+    int strListRowSize = strList.size();
+    int strListColumnSize = strList.at(0).size();
 
-    for (size_t i = 2; i < strList.size(); i=i+2)
-    {
-        std::vector<int> newIdxList;
-        for (size_t j = 0; j < idxList.size(); j++)
-        {
-            recursion(strList[i], idxList[j], newIdxList, totalTimeline);
-        }
-        idxList.clear();
-        idxList = newIdxList;
-        newIdxList.clear();
-        // std::cout << "split: ";
-        // for (size_t i = 0; i < idxList.size(); i++)
-        // {
-        //     std::cout << idxList.at(i) << ", ";
-        // }
-        // std::cout << std::endl;
-        
-    }
-    std::cout << "part 1 count: " << countSplit << std::endl;
-    std::cout << "part 2 total timeline count: " << idxList.size() << std::endl;
-    //testing
+    recursion_new(strList, strListRowSize, strListColumnSize, 2, idx_init, totalTimeline);
+
+    std::cout
+        << "part 2 total timeline count: " << totalTimeline << std::endl;
 
     return 0;
 }
